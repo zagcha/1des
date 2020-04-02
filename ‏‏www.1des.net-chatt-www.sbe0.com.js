abebac47a450ce9d3f2deff65caaee4c58e853b7
waleed-qaid <48764757+waleed-qaid@users.noreply.github.com>
2020-04-02 03:53:36 +0300

    Add files via upload

---
+++ ‏‏www.1des.net-chatt-www.sbe0.com.js
@@ -0,0 +1,28 @@
+﻿$(`<a id="des3" href="http://www.sbe0.com/" style="border-radius: 24px 0px 24px 0px;width:23%;margin:2px;" class="fl mini  fa  fa-facebook
+ btn btn-primary"> فيس بوك</a>
+<a id="des3" href="http://www.sbe0.com/" style="border-radius: 0px 24px 0px 24px;width:23%;margin:2px;" class="fl mini  fa   fa-star
+ btn btn-default"> ميز نفسك</a>
+<a id="des3" href="http://www.sbe0.com/" style="border-radius: 24px 0px 24px 0px;width:26%;margin:2px;" class="fl mini  fa fa-graduation-cap
+btn btn-primary"> قوانين موقع</a>
+<a id="des3" href="http://www.sbe0.com/" style="border-radius: 0px 24px 0px 24px;width:20%;margin:2px;" class="fl mini  fa fa-phone
+btn btn-default"> اتصل بنا</a>
+
+
+
+
+
+`).insertBefore('.nav-tabs');
+
+$(".u-pic").css("border-radius","3px");
+$( "button.btn.btn-primary" ).css( "border-radius", "15px" )
+
+$('.hand.nosel.fl.uzr.border').css({'margin': '1px 0',paddingTop: '2px',borderRadius: '5px'});
+$('.uzr.fl.corner.borderg').css({'margin': '0px 0',paddingTop: '2px',borderRadius: '5px'}); 
+
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value="" maxlength="12"><i class="fa fa-eye-slash" aria-hidden="true">`)
+$("[data-target=#wall]").attr('title',' مســآحه حره لأبداعاتكم ')
+
+$('.nav-tabs').addClass('fl').css('width','100%');
+$(`<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" href="">
+<link href="https://rawcdn.githack.com/waleed-qaid/1des/2ee5a9648b9873613e726605243fbd4d43af2cdc/‏‏www.1des.net-chatt-www.sbe0.com.css" rel="stylesheet" type="text/css" />
